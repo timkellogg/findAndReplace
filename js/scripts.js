b7b8c9313a7ac67f21e2dc90ce76d5538ee9a8be
timkellogg <tim.kellogg@gmail.com>
2015-09-11 10:47:15 -0700

    fix case sensitivity checking

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,23 @@ String.prototype.findAndReplace = function (find, replacement, sensitive) {
   var re = new RegExp(find, 'g' + i);
   return this.replace(re, replacement);
 };
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    var initialText = $('#initial-text').val();
+    var find        = $('#find').val();
+    var replacement = $('#replacement').val();
+    var sensitivity = $('#case-sensitive').prop('checked');
+    var result      = initialText.findAndReplace(find, replacement, sensitivity);
+
+    $('#result-container').show();
+    $('#result-text').text(result);
+    event.preventDefault();
+  });
+  $('#reset').click(function() {
+    $('#initial-text').val('');
+    $('#find').val('');
+    $('#replacement').val('');
+    $('#result-container').hide();
+  });
+});
