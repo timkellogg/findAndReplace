d21a3fe6eb7d0d0c408b8f8bc39e6acf23f11cd5
timkellogg <tim.kellogg@gmail.com>
2015-09-11 00:54:17 -0700

    handles multi-word strings

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe('findAndReplace', function() {
   it('should find and replace more than a single occurance', function() {
     expect("moo".findAndReplace('o', 'e')).to.equal('mee');
   });
+  it('should handle two or more word strings', function() {
+    expect("Hello world".findAndReplace('world', 'universe')).to.equal('Hello universe');
+  });
 });
