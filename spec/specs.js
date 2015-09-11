d42036185a57af1f5104a33d1803401763de14d7
timkellogg <tim.kellogg@gmail.com>
2015-09-11 01:02:04 -0700

    add ability to pass case-sensitivity

--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,10 @@ describe('findAndReplace', function() {
   it('should handle two or more word strings', function() {
     expect("Hello world".findAndReplace('world', 'universe')).to.equal('Hello universe');
   });
+  it('should allow the user the option to pass in true as a paremter to perform a case-insensitive search', function(){
+    expect('TtTt'.findAndReplace('T', 'M', true)).to.equal('MMMM');
+  });
+  it('should allow the user the option to pass in false paremter to perform the default sensitive search', function(){
+    expect('TtTt'.findAndReplace('T', 'M', false)).to.equal('MtMt');
+  });
 });
